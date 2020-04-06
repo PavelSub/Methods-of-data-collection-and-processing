f4a9de9768a1f102d1fe91767f317955e45be182
PavelSub <subbotinpu@gmail.com>
2020-04-06 15:46:37 +0300

    lesson6

---
+++ start_parse.py
@@ -0,0 +1,19 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from blogparse import settings
+# from blogparse.spiders.habr_news import HabrNewsSpider
+# from blogparse.spiders.avito import AvitoSpider
+from blogparse.spiders.instagram import InstagramSpider
+# from blogparse.spiders.www_zillow_com import WwwZillowComSpider
+import log_pass
+
+
+if __name__ == '__main__':
+    craw_settings = Settings()
+    craw_settings.setmodule(settings)
+    cr_process = CrawlerProcess(settings=craw_settings)
+    # cr_process.crawl(HabrNewsSpider)
+    # cr_process.crawl(AvitoSpider)
+    cr_process.crawl(InstagramSpider, logpass=(log_pass.INSTA_LOGIN, log_pass.INSTA_PWD))
+    # cr_process.crawl(WwwZillowComSpider)
+    cr_process.start()
